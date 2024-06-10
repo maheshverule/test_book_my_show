c42d674efb38f68667b4092643302f757b5a4539
maheshverule <143202737+maheshverule@users.noreply.github.com>
2024-06-10 19:18:49 +0530

    base url and locators configration

--- book_my_show.py
+++ book_my_show.py
@@ -7,24 +7,29 @@ import config
 
 city = config.CITY
 filter_lang = config.LANG
+url = config.URL
+search_city_textbox = config.SEARCH_CITY_TEXTBOX
+home_screen_city_display = config.HOME_SCREEN_DISPLAY_SELCTED_CITY
+home_screen_search_bar = config.HOME_SCREEN_SEARCH_BAR
+language_filter = config.LANGUAGE_FILTER
 
 options = Options()
 options.add_experimental_option("detach",True)
 driver = webdriver.Chrome(options = options)
-driver.get("https://in.bookmyshow.com/")
+driver.get(url)
 print(driver.title)
-element = driver.find_element(By.XPATH,"//input[@placeholder = 'Search for your city']")
+element = driver.find_element(By.XPATH,search_city_textbox)
 element.send_keys(config.CITY)
 time.sleep(2)
 element.send_keys(Keys.ENTER)
 driver.implicitly_wait(5)
-City = driver.find_element(By.XPATH,"//span[@class ='bwc__sc-1nbn7v6-10 cntvL ellipsis' and text()]")
+City = driver.find_element(By.XPATH,home_screen_city_display)
 print(City.text)
 assert city == City.text
 try:
-    driver.find_element(By.ID,'4').click()
+    driver.find_element(By.ID,home_screen_search_bar).click()
     time.sleep(5)
-    driver.find_element(By.XPATH,"//div[@class ='bwc__sc-m1rlyj-3 bwc__sc-3t17w7-25 bzIewT']").send_keys(filter_lang).click()
+    driver.find_element(By.XPATH,language_filter).send_keys(filter_lang).click()
 
 except BaseException:
     print("Execution not complted need to handle the error")
